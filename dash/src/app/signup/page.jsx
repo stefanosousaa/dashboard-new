3bc324c3ae6c8797e48f1eeaceb4e73e47b33268
Stefano Sousa <stefanosousa@gmail.com>
2024-02-23 22:51:19 -0300

    sign signup

--- dash/src/app/signup/page.jsx
+++ dash/src/app/signup/page.jsx
@@ -1,13 +1,13 @@
 "use client"
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import React from 'react'
+import { useRouter } from 'next/navigation'
 
-export default function Home() {
+export default function SignUp() {
 
-  const router = useRouter();
+    const router = useRouter();
   return (
     <>
-      <div className="flex justify-center mt-16">
+      <div className="flex justify-center mt-8">
         <div style={{minWidth: "30%"}}>
         <div className="shadow-lg flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -17,7 +17,7 @@ export default function Home() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Create a new account
           </h2>
         </div>
 
@@ -44,16 +44,30 @@ export default function Home() {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
+                
               </div>
               <div className="mt-2">
                 <input
                   id="password"
                   name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="cpassword" className="block text-sm font-medium leading-6 text-gray-900">
+                  Confirm Password
+                </label>
+                
+              </div>
+              <div className="mt-2">
+                <input
+                  id="cpassword"
+                  name="cpassword"
                   type="password"
                   autoComplete="current-password"
                   required
@@ -67,19 +81,20 @@ export default function Home() {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Sign up
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
+            Already registred?{' '}
             <span className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 cursor-pointer"
-            onClick={()=>{router.push("/signup")}}
+            onClick={() => router.push("/") }
             >
-              Start a 14 day free trialx
-              </span>
-              
+              Sign In
+
+            </span>
+            
             
           </p>
         </div>
@@ -89,5 +104,5 @@ export default function Home() {
 
       
     </>
-  );
+  )
 }
